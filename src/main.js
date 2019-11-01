83db8e3899cf50562acbf3115b9534f932037c57
wss <wussdyx@163.com>
2019-11-01 21:26:35 +0800

    制作首页的Header和Tabbar

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-// 入口文件
-console.log('ok')
+import Vue from 'vue'
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header);
+
+import app from './App.vue'
+import './lib/mui/css/mui.css'
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
