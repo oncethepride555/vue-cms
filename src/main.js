f4f4da53e35776eeb0ffc8cb0d066e9b9c1888ad
wss <wussdyx@163.com>
2019-11-01 22:03:52 +0800

    try

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+// 按需导入
 import { Header } from 'mint-ui'
 Vue.component(Header.name, Header);
-
+// 导入 App 根组件
 import app from './App.vue'
+
 import './lib/mui/css/mui.css'
 
 var vm = new Vue({
